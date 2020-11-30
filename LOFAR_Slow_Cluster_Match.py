9a4b6e5ed9781c259c928a21c7a7359ab5a5b082
kellie.devos@gmail.com <kellie.devos@gmail.com>
2020-11-30 11:21:11 +0000

    no message

--- LOFAR_Slow_Cluster_Match.py
+++ LOFAR_Slow_Cluster_Match.py
@@ -16,58 +16,71 @@ from astropy.cosmology import FlatLambdaCDM
 cosmo = FlatLambdaCDM(H0=68, Om0=0.315)
 
 # Read cluster catalogues and radio-optical data
-WHL = Table.read('Full WHL Cluster Catalogue')
+Clus = Table.read('Full WHL Cluster Catalogue') # Input can be changed depending on cluster catalogue
 Rad = Table.read('Radio source data')
 
-# Only keep radio sources that have a reasonable radio-optical offset
-# Rad = Rad[(Rad['Radio-Optical Offset'] >= 1) & (Rad['Radio-Optical Offset'] <= 100)]
-
 # Put RA and DECs into SkyCoord co ordinates
-WHLco = SkyCoord(WHL['Cluster RA'], WHL['Cluster DEC'], unit='deg')
+Clusco = SkyCoord(Clus['Cluster RA'], Clus['Cluster DEC'], unit='deg')
 Radco = SkyCoord(Rad['Optical RA'], Rad['Optical DEC'], unit='deg')
 
 # Calculate co-moving distance of all z values in radio-optical data
 comDist = np.array(cosmo.comoving_distance(Rad['Optical z'])/u.Mpc)
 
+'''
+What I would like to do in the below code is:
+    - Find Delta z = (z_gal - z_clus) / (1 + z_gal) for every cluster for each
+    radio-optical source
+    - When |Delta z| < 0.04, Find the physical distance on the sky in Mpc, at
+    the z of the radio-optical source
+    - Find the radio-optical/cluster match with the minimum physical distance
+    on the sky that is < 15Mpc and within |Delta z| < 0.04, for each
+    radio-optical source
+    
+I would prefer not to do this with a nested for loop but I'm not sure how else
+to do it! I have a faster alternative for cluster matching which is in the
+Optical_Cluster_match module, but my supervisors would prefer for me to use
+this method as it follows the same method used in a paper I am looking at.
 
+Please let me know if you know how to make this faster!
+'''
 #%%
 
 # Create empty arrays to hold cluster IDs, Delta z values, and dist values
-WHLIDs = np.array([])
-WHLallDz = np.array([])
-WHLallDist = np.array([])
+ClusIDs = np.array([])
+ClusAllDz = np.array([])
+ClusAllDist = np.array([])
 
 # Run through all radio-optical values
 for i in range(0, len(Rad['Optical RA'])):
     # Set dist values that are outside of the constraint of being < 15Mpc
-    WHLdist = 15
+    Clusdist = 15
     # Run through length of all WHL values
-    for j in range(0, len(WHL['Cluster RA'])):
+    for j in range(0, len(Clus['Cluster RA'])):
         # Calculate Delta z values between current optical source and cluster
-        WHLdz = lf.z_diff_calc(Rad['Optical z'][i], WHL['Cluster z'][j])
+        Clusdz = lf.z_diff_calc(Rad['Optical z'][i], Clus['Cluster z'][j])
         # See if absolute value of Delta z is less than 0.04
-        if abs(WHLdz) < 0.04:
+        if abs(Clusdz) < 0.04:
             # If so, calculate distance on sky between cluster and optical source
-            # WHLoffset = np.radians((Radco[i].separation(WHLco[j])).degree) # Find offset between RA and DEC
-            WHLoffset = np.radians(lf.offset(WHL['Cluster RA'][j], WHL['Cluster DEC'][j],
+            # Clusoffset = np.radians((Radco[i].separation(Clusco[j])).degree) # Find offset between RA and DEC
+            Clusoffset = np.radians(lf.offset(Clus['Cluster RA'][j], Clus['Cluster DEC'][j],
                                              Rad['Optical RA'][i], Rad['Optical DEC'][i])/3600)
-            WHLdistNew = WHLoffset*comDist[i] # Find distance on sky at optical z
+            ClusdistNew = Clusoffset*comDist[i] # Find distance on sky at optical z
             # Check if this new distance is less than 15Mpc or the previous value
-            if WHLdistNew < WHLdist:
+            if ClusdistNew < Clusdist:
                 # If so, save it and its corresponding Delta z and index
-                WHLdist = WHLdistNew
-                WHLdzKeep = WHLdz
-                WHLind = j
+                Clusdist = ClusdistNew
+                ClusdzKeep = Clusdz
+                Clusind = j
     # Once all j indices are run through for single i, check to see if any matches
     # were found that fit the constraints, and if so append it, but if not append nan
-    if WHLdist < 15:
-        WHLIDs = np.append(WHLIDs, WHL['Cluster ID'][WHLind])
-        WHLallDz = np.append(WHLallDz, WHLdzKeep)
-        WHLallDist = np.append(WHLallDist, WHLdist)
+    if Clusdist < 15:
+        ClusIDs = np.append(ClusIDs, Clus['Cluster ID'][Clusind])
+        ClusallDz = np.append(ClusAllDz, ClusdzKeep)
+        ClusallDist = np.append(ClusAllDist, Clusdist)
     else:
-        WHLIDs = np.append(WHLIDs, np.nan)
-        WHLallDz = np.append(WHLallDz, np.nan)
-        WHLallDist = np.append(WHLallDist, np.nan)
+        ClusIDs = np.append(ClusIDs, np.nan)
+        ClusallDz = np.append(ClusallDz, np.nan)
+        ClusallDist = np.append(ClusallDist, np.nan)
             
                 
                 
